85cdbefa9b50910a082825dcc1038a7e9ec47cf8
ROOT\ramdhas.ueikattan <ramdhas.ueikattan@syncfusion.com>
2017-11-15 10:51:42 +0530

    columns added in ts

--- app/grid.component.ts
+++ app/grid.component.ts
@@ -1,24 +1,27 @@
 import { Component, OnInit, ViewChild, Input } from '@angular/core';
+import { Column } from '@syncfusion/ej2-grids';
 
 /**
  * j-nord- github issue#7
  */
 @Component({
     selector: 'my-grid',
-    template: ` <ej-grid #grid [locale]='de-DE' [dataSource]='mySource'>
-                <e-columns>
-                    <e-column field='CustomerID' headerText='Customer ID' width=150 ></e-column>
-                    <e-column field='Freight' headerText='Freight' [format]='form' textAlign='right' width=150></e-column>
-                    <e-column field='ShipCity' headerText='Ship City' width=150></e-column>
-                    <e-column field='ShipName' headerText='Ship Name' width=150></e-column>
-                </e-columns>
+    template: ` <ej-grid #grid [locale]='de-DE' [dataSource]='mySource' [columns]='columns'>
                 </ej-grid> `
 })
 export class GridComponent implements OnInit {
     @ViewChild('grid')
     public grid;
     @Input() mySource: any;
+
+    public columns: Column[];
+
     ngOnInit(){
-        //
+        this.columns = <Column[]> [
+            { field: 'OrderID', headerText: 'Order ID', textAlign: 'right', width: 120, type: 'number' },
+            { field: 'CustomerID', width: 140, headerText: 'Customer ID', type: 'string' },
+            { field: 'Freight', headerText: 'Freight', textAlign: 'right', width: 120, format: 'C2' },
+            { field: 'ShipCountry', headerText: 'Ship Country', width: 140 }
+        ];
     }
 }
